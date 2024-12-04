c5e335d1b77ac51546897f40d3a124e16712d3d3
wina-ui <dhwang8@depaul.edu>
2024-12-04 14:12:15 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Welcome to my portfolio!");
+
+// Log a message to the console
+console.log("JavaScript is running!");
+
+// Example: Alert when a specific section is clicked
+document.querySelector("#about").addEventListener("click", () => {
+  alert("You clicked on the About section!");
+});
